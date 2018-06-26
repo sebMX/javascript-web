0c5b19d597153f11ddddc62dcb0b23f39588742c
sebMX <seb.massonneau@gmail.com>
2018-06-26 22:06:20 +0200

    Ajout contenu cours

--- chapitre_2/js/cours.js
+++ chapitre_2/js/cours.js
@@ -50,3 +50,63 @@ console.log(document.querySelectorAll("#antiques > .existe"));	// Affiche 1
 // Le premier paragraphe
 console.log(document.querySelector("p"));
 
+
+// Obtenir des informations sur les éléments
+
+// Le contenu HTML
+
+// Le contenu HTML de l'élément identifié par "contenu"
+console.log(document.getElementById("contenu").innerHTML);
+
+
+// Le contenu textuel
+
+// Le contenu textuel de l'élément identifié par "contenu" (sans balise)
+console.log(document.getElementById("contenu").textContent);
+
+
+// Les attributs
+
+// L'attribut href du premier lien
+console.log(document.querySelector("a").getAttribute("href"));
+
+// Certains attributs sont directement accessibles sous la forme de propriétés. 
+// C'est notamment le cas pour les attributs id, href et value.
+
+// L'identifiant de lma première liste
+console.log(document.querySelector("ul").id);
+
+// L'attribut href du premier lien
+console.log(document.querySelector("a").href);
+
+
+// On peut vérifier la présence d'un attribut sur un élément grâce à la méthode hasAttribute, 
+// comme dans l'exemple ci-après.
+
+if (document.querySelector("a").hasAttribute("target")) {
+	console.log("Le premier lien possède l'attribut target");
+} else {
+	console.log("Le premier lien ne possède pas l'attribut target");
+}
+
+
+// Les classes
+
+// Dans une page web, une balise peut posséder plusieurs classes. 
+// La propriéte classList permet de récupérer la liste des classes d'un élément du DOM. 
+// Elle s'utilise comme un tableau.
+
+// Liste des classes de l'élément identifié par "antiques"
+
+var classes = document.getElementById("antiques").classList;
+console.log(classes.length);	// Affiche 1 : l'élément possède une seule classe
+console.log(classes[0]);	// Affiche "merveilles"
+
+// Vous avez aussi la possibilité de tester la présence d'une classe dans un élément 
+// en appelant la méthode contains sur la liste des classes.
+
+if (document.getElementById("antiques").classList.contains("merveille")) {
+	console.log("L'élément identifié par antiques possède la classe merveille");
+} else {
+	console.log("L'élément identifié par antiques ne possède pas la classe merveille");
+}
