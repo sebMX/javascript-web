2c30115d59813d99760779e13e0343a834b3645a
sebMX <seb.massonneau@gmail.com>
2018-06-28 11:02:44 +0200

    Ajout chapitre_3

---
+++ chapitre_3/js/cours.js
@@ -0,0 +1,11 @@
+// Modifiez la structure de la page
+
+
+// Modifier un élément existant
+
+// Le contenu HTML
+// La propriété innerHTML peut être utilisée pour modifier 
+// le contenu HTML d'un élément du DOM. 
+
+// Modification du contenu HTML de la liste : ajout d'un langage
+document.getElementById("langages").innerHTML += '<li id="c">C</li>';
